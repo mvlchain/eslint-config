a2225085afd2bf6170a2bbd54453718835b7c708
Yongjun Park <pyjun02@gmail.com>
2022-08-10 14:34:49 +0900

    Add rules

---
+++ react.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: ['react', 'react-hooks'],
+  extends: ['./index', 'plugin:react/recommended', 'plugin:react/jsx-runtime', 'plugin:react-hooks/recommended'],
+  rules: {
+    'react/jsx-no-comment-textnodes': 'off',
+    'react/jsx-no-duplicate-props': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+  },
+};
